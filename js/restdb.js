76a4e915775d7dcecae909395604e4ecd81afa7f
Lee Zhi Wei <zhiweilee.com@gmail.com>
2023-02-02 19:48:07 +0800

    somewhat implement API

--- js/restdb.js
+++ js/restdb.js
@@ -1,33 +1,34 @@
-var settings = {
+function getquestions(){
+  var settings = {
     "async": true,
     "crossDomain": true,
     "url": "https://assignment2-3ebe.restdb.io/rest/question",
     "method": "GET",
     "headers": {
       "content-type": "application/json",
-      "x-apikey": "<your CORS apikey here>",
+      "x-apikey": "63d1fd77a95709597409cfa6",
       "cache-control": "no-cache"
     }
   }
-  
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    respget = response;
   });
-  var jsondata = {"field1": "xyz","field2": "abc"};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://assignment2-3ebe.restdb.io/rest/question",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json",
-    "x-apikey": "<your CORS apikey here>",
-    "cache-control": "no-cache"
-  },
-  "processData": false,
-  "data": JSON.stringify(jsondata)
 }
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+function sendquestion(data){
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://assignment2-3ebe.restdb.io/rest/question",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "<your CORS apikey here>",
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(data)
+  }
+  $.ajax(settings).done(function (response) {
+    respsend = response;
+  });
+}
