7e5059dac354b3d630710bbd00ae17a507f24815
Lee Zhi Wei <zhiweilee.com@gmail.com>
2023-02-07 22:54:55 +0800

    create page working

--- js/create.js
+++ js/create.js
@@ -18,11 +18,16 @@ function getansandsubmit(){
     for (let qn = 1; qn <= 10; qn++){
         let question = {"numb":qn}
         let answer = form.find(`#q${qn}_ans`).val();
+        let questionask = form.find(`#q${qn}_qn`).val();
+        if (questionask == ""){
+            questionwithnoans += `<p><b>Question ${qn} has no question.</b></p>`;
+            break;
+        }
+        question["question"] = questionask;
         question["answer"] = answer;
         let array = [];
         for (let op = 1; op <= 4; op++){
             let options = form.find(`#q${qn}_op${op}`).val();
-            console.log(options);
             array.push(options);
         }
         if (!array.includes(answer)){
@@ -36,15 +41,22 @@ function getansandsubmit(){
         return false;
     }
     console.log(arr);
+    return true;
 }
+
 form.find('.create_btn').click( () => {
     if (!getansandsubmit()){
         return;
-    };
+    }
     let namecreate = form.find("#q_cname").val();
     if (namecreate == ""){
         infopanel.html("<p><b>CreatorName cannot be empty</b></p>");
         return;
     }
-    sendquestion({"QuestionID": qnlist.length + 1,"QuestionList":arr,"CreatorName":namecreate});
+    let id = qnlist.length + 1;
+    console.log(id);
+    sendquestion({"QuestionID": id,"QuestionList":finalarr,"CreatorName":"Lee8219"});
+    infopanel.html(`<p><b>Your QuizID is ${id}</b></p><br><p>Wait until browser refreshes before you create another form./p>`);
+    form.find('.create_btn').prop('disabled', true);
+    setTimeout(() => {location.reload()},25000);
 });
