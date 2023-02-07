d64bc9500ce77e006da35212260aa69ea950f808
Lee Zhi Wei <zhiweilee.com@gmail.com>
2023-02-07 22:00:01 +0800

    some input validation.

--- js/create.js
+++ js/create.js
@@ -1,8 +1,21 @@
+var respget;
+var qnlist;
+const infopanel = $('.info');
+getquestions();
+function waitforqns(){
+  if (respget !== undefined){
+    qnlist = respget;
+  }
+  else{
+    setTimeout(waitforqns, 250);
+  }
+}
+waitforqns();
 const form = $('form');
 let arr = [];
+let questionwithnoans = ``;
 function getansandsubmit(){
     for (let qn = 1; qn <= 10; qn++){
-        console.log(`Question ${qn}`);
         let question = {"numb":qn}
         let answer = form.find(`#q${qn}_ans`).val();
         question["answer"] = answer;
@@ -12,13 +25,26 @@ function getansandsubmit(){
             console.log(options);
             array.push(options);
         }
+        if (!array.includes(answer)){
+            questionwithnoans += `<p><b>Question ${qn}'s answer is not in options.</b></p><br>`;
+        }
         question["options"] = array;
         arr.push(question);
+    }
+    if (questionwithnoans.length != 0){
+        infopanel.html(questionwithnoans);
+        return false;
     }
     console.log(arr);
 }
 form.find('.create_btn').click( () => {
-    getansandsubmit();
+    if (!getansandsubmit()){
+        return;
+    };
     let namecreate = form.find("#q_cname").val();
-    sendquestion({"QuestionID": respget.length + 1,"QuestionList":arr,"CreatorName":namecreate});
+    if (namecreate == ""){
+        infopanel.html("<p><b>CreatorName cannot be empty</b></p>");
+        return;
+    }
+    sendquestion({"QuestionID": qnlist.length + 1,"QuestionList":arr,"CreatorName":namecreate});
 });
