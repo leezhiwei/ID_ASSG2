49235eaf6aa4b71a25daa08ab3b5a85bffa05fce
Lee Zhi Wei <zhiweilee.com@gmail.com>
2023-02-10 00:24:06 +0800

    remove document body from script.js

--- js/script.js
+++ js/script.js
@@ -322,10 +322,3 @@ function queCounter(index){
 	let totalQueCounTag = "<span><p>"+ index +"</p> of <p>"+ questions.length +"</p> Questions</span>";
 	bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom question counter
 }
-let x = document.body;
-let color = ["fuchsia", "aqua", "limegreen", "chartreuse","silver","cornsilk"];
-setInterval(function() {
-	for(let y = 0; y < 4; y++){
-		x.style.backgroundColor = color[Math.floor(Math.random() * 3)];  
-	}
-}, 1111);
